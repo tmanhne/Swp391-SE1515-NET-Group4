f164a5f395664b690423922137e00a5737bdfe9f
tmanhne <manh.t.tran@akadon.com.vn>
2022-02-09 16:04:36 +0700

    codebase

--- src/java/controller/util/dummyDAO.java
+++ src/java/controller/util/dummyDAO.java
@@ -9,13 +9,6 @@ package controller.util;
  *
  * @author Hfyl
  */
-<<<<<<< HEAD:src/java/controller/util/dummyDAO.java
 public class dummyDAO {
     
-=======
-public class admin {
-    public static void main(String[] args) {
-        System.out.println("Đm thằng mạnh ngu vkl :))");
-    }
->>>>>>> 68d24fdde873264374641e2587029d8b08421843:src/java/Model/admin.java
 }

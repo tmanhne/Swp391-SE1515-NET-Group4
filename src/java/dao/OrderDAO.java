28f21ec2b98ba65e88b76b8cbac71f349fa849b7
dult6331 <88769860+dult6331@users.noreply.github.com>
2022-03-15 10:38:28 +0700

    Update OrderDAO.java

--- src/java/dao/OrderDAO.java
+++ src/java/dao/OrderDAO.java
@@ -87,7 +87,7 @@ public class OrderDAO extends DBConnection {
                 + "           ,[PaymentMethod]) "
                 + "           OUTPUT INSERTED.[OrderID] "
                 + "     VALUES "
-                + "            ('OD'+CAST((SELECT TOP(1)CAST(SUBSTRING(OrderID,3,10)AS INT)+1 FROM Orders ORDER BY OrderID DESC) AS VARCHAR(10)), "
+                + "            ('OD'+CAST((SELECT IIF(Count(*)=0,1,(SELECT TOP(1)CAST(SUBSTRING(OrderID,3,10)AS INT)+1 FROM Orders ORDER BY OrderID DESC)) FROM Orders) AS VARCHAR(10)), "
                 + "            ? , "
                 + "            ? , "
                 + "            ? , "
